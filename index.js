e8d58be6e4bc4faf134afa464ff8c138b9409f10
Alex Staley <alexstaley@Alexs-MBP-2.home>
2020-03-31 21:13:48 -0400

    commented

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const CMS_Prompt = require("./cms-inquirer");
 
 //const queries = new Queries();
 const app = new CMS_Prompt();
-console.log(
-  "\n\n========================\nNaomi's EMS\n========================"
+    console.log(
+
+      //My code starts 
+      "\n\n========================\nNaomi's EMS\n========================"
 );
 app.runPrompt();
